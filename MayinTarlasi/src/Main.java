673d1a1076ee4cb0005ba3f5093360e968c3d910
clkmerve <merve_celikk12@hotmail.com>
2022-09-28 14:52:17 +0300

    java uygulama
    java uygulama

---
+++ MayinTarlasi/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void  main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		int row,column;
+		System.out.println("Mayýn Tarlasý ");
+		System.out.println(" Oynamak istediðiniz boyutlarý giriniz!");
+		System.out.print("Satýr sayýsý: ");
+		row=scan.nextInt();
+		System.out.print("Sütün sayýsý: ");
+		column=scan.nextInt();
+		
+		MayinTarlasi mayin = new MayinTarlasi(row,column);
+		mayin.run();//run metodu çalýþtýr
+	}
+
+}
